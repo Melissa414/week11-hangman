431ff4b19287e4cb54b12d446e673f48fa4bbb1f
Melissa414 <Missy41491@gmail.com>
2016-10-15 20:52:56 -0400

    prompts you now to type in a letter

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
 var inquirer = require("inquirer");
-var require = ("./word.js", "./letter.js", "game.js");
-console.log("This works, baby!")
+var require = ("./word.js", "./letter.js", "./game.js");
+console.log("\n------------------------------------");
+console.log("   Let's play a game...")
+console.log("------------------------------------");
 
-function Hangman(guess, answer, word, correct, wrong, game) {
+function Hangman(guess, answer, word, correct, wrong, game, score) {
     this.guess = guess,
         this.answer = answer,
         this.word = word,
         this.correct = correct,
         this.wrong = wrong,
         this.game = game,
+        this.score = score,
 
-        this.guess = function() {
-            if (guess == correct) {
+        this.game = function() {
+            if (guess === correct) {
+                //only 5 words to guess, currently
                 for (var i = 0; i < 5; i++) {
-                    this.correct++;
-                    console.log("Correct, what will be your next guess?")
-                    console.log("\n------------------------------------")
-                } else {
-                    wrong++;
-                    console.log("Try again.")
-                    console.log("\n------------------------------------")
+                    //adds one point to the winning score
+                    correct++;
+                    console.log("Hey, " + this.name + "Correct, what will be your next guess?");
+                    console.log("\n------------------------------------");
                 }
+            } else {
+                //adds one point to the losing score
+                wrong++;
+                console.log(this.name + ",try again.")
+                console.log("\n------------------------------------")
             }
         }
-        this.word = function() {
-            
-        }
+
 }
+
+    inquirer.prompt([
+
+            {
+                type: "input",
+                message: " Type in a letter: ",
+                name: "entering"
+            },
+])
+// var loop = 0;
+// var letterGuess = [];
+// var letter = [];
+
+    lettersMatched = function(correct) {
+        if (letter == true) {
+            letter.push(answer.correct);
+            console.log("Correct!");
+            console.log("\n------------------------------------")
+                }
+            }
--- word.js
+++ word.js
@@ -7,3 +7,10 @@ var options = {
           "phlegm",
   }
 }
+
+var letterInWord = [],
+var matchedLetters: [],
+var guessedLetters: [],
+var guessesLeft: 0,
+var totalGuesses: 0,
+var wins = [],
